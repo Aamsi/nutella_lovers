5826fec75519f4223dcb494e7b2a8bc9ccf6d596
Aamsi <ismaelouali45@gmail.com>
2020-07-30 22:24:49 +0200

    Creates welcome page

--- nutella_lovers/settings.py
+++ nutella_lovers/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nutella_lovers',
+    'substitute',
+    'user'
 ]
 
 MIDDLEWARE = [
--- nutella_lovers/urls.py
+++ nutella_lovers/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-import substitute.views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('substitute/', include('substitute.urls'))
+    path('', views.index),
 ]

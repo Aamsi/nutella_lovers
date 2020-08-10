76d3a776d9e43da38f93a2da90e3a1db25f773b3
Aamsi <ismaelouali45@gmail.com>
2020-08-10 21:57:51 +0200

    Takes off useless lines of code

--- user/views.py
+++ user/views.py
@@ -10,10 +10,7 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(request, username=username, password=password)
+            user = form.save()
             login(request, user)
             return redirect('welcome')
     else:

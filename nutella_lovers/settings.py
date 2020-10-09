c6f4369303c495c2b82667b95d2b140bab163233
Aamsi <ismaelouali45@gmail.com>
2020-10-09 20:24:50 +0200

    ajoute shell+ dans les installed apps

--- nutella_lovers/settings.py
+++ nutella_lovers/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'django_extensions',
     'crispy_forms',
     'nutella_lovers',
     'substitute',

b986bb10e05d2eeeffb09379b718c41d2936c70a
Aamsi <ismaelouali45@gmail.com>
2020-10-09 20:24:19 +0200

    refais la view de recherche des substituts

--- substitute/views.py
+++ substitute/views.py
@@ -2,44 +2,27 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.postgres.search import SearchVector
 from django.http import HttpResponse
+from django.db.models import Count, Q
 
-from .models import Products
+from .models import Products, Categories
 
 
 def search_substitute(request):
-    if request.method == 'GET':
-        search_input = request.GET['q']
+    if request.method != 'GET':
+        return
 
-        products_found = Products.objects.annotate(
-            search=SearchVector('name', 'details', 'categories__name')
-        ).filter(search=search_input)
+    search_input = request.GET['q'] # TODO: Virer le q
 
-        categories_pk = []
-        for product in products_found:
-            for category_pk in product.categories.values_list('pk', flat=True):
-                categories_pk.append(category_pk)
+    products_found = Categories.objects.annotate(
+        search=SearchVector('products__name')
+    ).filter(search=search_input)
 
-        categories_pk = sorted(set(categories_pk))
+    effective_products = Products.objects.annotate(
+            common_category=Count("categories", filter=Q(categories__in=products_found)))\
+            .filter(common_category__gt=5)\
+            .order_by("nutriscore", "-common_category")
 
-        # Pas trouve de moyen pour prendre directement les produits qui ont au moins X categories dans la liste
-        products = Products.objects.filter(categories__in=categories_pk)
-        products = sorted(
-            set(products),
-            key=lambda prod: prod.pk
-        )
-
-        effective_products = []
-        for product in products:
-            i = 0
-            for cat in product.categories.values_list("pk", flat=True):
-                if cat not in categories_pk:
-                    continue
-                i += 1
-            if i >= 5:
-                effective_products.append(product)
-
-        return HttpResponse([product.name + "</br>" for product in effective_products])
-
-        
-        
-        
+    return render(request, 'substitute/search_result.html', {
+        'products': effective_products
+    })
+  

7b7e636964e685f1c1c39373711e57c8609536ec
Aamsi <ismaelouali45@gmail.com>
2020-10-14 18:35:36 +0200

    ajoute la page des favoris et l'oubli possible des substituts

--- substitute/urls.py
+++ substitute/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('search/', views.search_substitute, name='search'),
+    path('favs/', views.my_favs, name='favs'),
     path('save/', views.save_substitute, name='save'),
+    path('forget/', views.forget_substitute, name='forget'),
 ]
--- substitute/views.py
+++ substitute/views.py
@@ -6,13 +6,11 @@ from django.db.models import Count, Q
 
 from .models import Products, Categories, Favorites, User
 
-
+@login_required
 def search_substitute(request):
     if request.method != 'GET':
         return
-
     search_input = request.GET['q']
-
     products_found = Categories.objects.annotate(
         search=SearchVector('products__name')
     ).filter(search=search_input)
@@ -21,19 +19,51 @@ def search_substitute(request):
             common_category=Count("categories", filter=Q(categories__in=products_found)))\
             .filter(common_category__gt=5)\
             .order_by("nutriscore", "-common_category")[:20]
+    favs = []
+    if request.user.is_authenticated:
+        favs = request.user.favs.values_list('product_replacement__id', flat=True)
     return render(request, 'substitute/search_result.html', {
-        'products': effective_products
+        'products': effective_products,
+        'favs': favs
     })
+
+@login_required
+def my_favs(request):
+    if request.method != 'GET':
+        return
+    favs = Favorites.objects.filter(fav_user=request.user)\
+    .values_list('product_replacement', flat=True)
+    fav_products = Products.objects.filter(id__in=favs)
+    return render(request, 'substitute/favs.html', {
+        'favs': fav_products
+    })
+
 
 @login_required
 def save_substitute(request):
     if request.method != 'GET':
         return
     product_id = int(request.GET['product_id'])
-    print(product_id)
     saved_prod = Products.objects.get(id=product_id)
+    if not saved_prod:
+        return JsonResponse({'success': False})
     Favorites.objects.get_or_create(
         fav_user=request.user,
         product_replacement=saved_prod
     )
     return JsonResponse({"success": True})
+
+@login_required
+def forget_substitute(request):
+    if request.method != 'GET':
+        return
+    product_id = int(request.GET['product_id'])
+    forgot_prod = Favorites.objects.get(
+        fav_user=request.user,
+        product_replacement=Products.objects.get(id=product_id)
+    )
+    if not forgot_prod:
+        return JsonResponse({'success': False})
+    forgot_prod.delete()
+    return JsonResponse({'success': True})
+
